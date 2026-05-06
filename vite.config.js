abf8f2e4e185b7cc0fc0c164e2c1b8e6537bb09d
piccardino <zeuspiccardinoo@gmail.com>
2026-05-06 17:13:41 +0200

    #

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Floating-Island/',
   build: {
     rollupOptions: {
       input: {
